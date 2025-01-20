06c6c0c5d87c57290b8ea8667abe1b4684a93f75
Dias <dias.atudinov@gmail.com>
2025-01-20 15:02:43 +0500

    first commit

---
+++ Seven Feathers Online/Seven Feathers Online/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seven Feathers Online
+//
+//  Created by Dias Atudinov on 20.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seven Feathers Online/Seven Feathers Online/Seven_Feathers_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seven_Feathers_OnlineApp.swift
+//  Seven Feathers Online
+//
+//  Created by Dias Atudinov on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Seven_Feathers_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
