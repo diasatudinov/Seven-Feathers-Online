154671f077a626c9888f054c7a7985f085575d58
Dias <dias.atudinov@gmail.com>
2025-01-22 00:05:29 +0500

    Splash screen add

--- Seven Feathers Online/Seven Feathers Online/Modules/Menu/SplashScreen.swift
+++ Seven Feathers Online/Seven Feathers Online/Modules/Menu/SplashScreen.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    
+                    Spacer()
+                    
+                }
+                Spacer()
+                
+                
+            }
+            
+            VStack {
+                Spacer()
+                
+                Image(.logo1)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                    .padding(.bottom, 20)
+                
+                Spacer()
+            }
+        }.background(
+            Color.appSkyBlue
+//            Image(.bg1)
+//                .resizable()
+//                .edgesIgnoringSafeArea(.all)
+//                .scaledToFill()
+            
+        )
     }
 }
-
 #Preview {
     SplashScreen()
 }
