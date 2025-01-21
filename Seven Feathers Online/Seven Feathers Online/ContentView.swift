8e15004aaa408cabaa6d70442f7ab377bec11825
Dias <dias.atudinov@gmail.com>
2025-01-22 00:04:51 +0500

    Device info add

--- Seven Feathers Online/Seven Feathers Online/ContentView.swift
+++ Seven Feathers Online/Seven Feathers Online/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+          
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            
+            
         }
-        .padding()
+        .background(
+            ZStack {
+                Color.appSkyBlue
+                Image(.bg1)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            }.ignoresSafeArea()
+        )
     }
 }
 
--- Seven Feathers Online/Seven Feathers Online/Managers/DeviceInfo.swift
+++ Seven Feathers Online/Seven Feathers Online/Managers/DeviceInfo.swift
@@ -1,3 +1,11 @@
+//
+//  DeviceInfo.swift
+//  Seven Feathers Online
+//
+//  Created by Dias Atudinov on 20.01.2025.
+//
+
+
 import UIKit
 
 class DeviceInfo {
